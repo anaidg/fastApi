5edc285ec13b3d743006f3e93b02851cda38535f
Diana Milena Toro Arrieta <dmtoro@sura.com.co>
2021-04-04 23:41:21 -0500

    se agrega modelo multiequeta

---
+++ algoritms/multiEtiqueta.py
@@ -0,0 +1,64 @@
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.svm import LinearSVC
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.preprocessing import MultiLabelBinarizer
+import numpy as np
+import pickle
+
+X_train = np.array(["new york is a hell of a town",
+                    "new york was originally dutch",
+                    "the big apple is great",
+                    "new york is also called the big apple",
+                    "nyc is nice",
+                    "people abbreviate new york city as nyc",
+                    "the capital of great britain is london",
+                    "london is in the uk",
+                    "london is in england",
+                    "london is in great britain",
+                    "it rains a lot in london",
+                    "london hosts the british museum",
+                    "new york is great and so is london",
+                    "i like london better than new york"])
+y_train_text = [["new york"],["new york"],["new york"],["new york"],["new york"],
+                ["new york"],["london"],["london"],["london"],["london"],
+                ["london"],["london"],["new york","london"],["new york","london"]]
+
+X_test = np.array(['nice day in nyc',
+                   'welcome to london',
+                   'london is rainy',
+                   'it is raining in britian',
+                   'it is raining in britian and the big apple',
+                   'it is raining in britian and nyc',
+                   'hello welcome to new york. enjoy it here and london too'])
+target_names = ['New York', 'London']
+
+
+mlb = MultiLabelBinarizer()
+Yt = mlb.fit_transform(y_train_text)
+
+classifier = Pipeline([
+    ('vectorizer', CountVectorizer()),
+    ('tfidf', TfidfTransformer()),
+    ('clf', OneVsRestClassifier(LinearSVC()))])
+
+classifier.fit(X_train, Yt)
+pickle.dump(classifier, open('./models/multietiquetas.pkl', 'wb'))
+
+
+loaded_model = pickle.load(open('./models/multietiquetas.pkl', 'rb'))
+result = loaded_model.predict(X_test)
+
+predicted = loaded_model.predict(X_test)
+
+def getmlb(predicted):
+    return mlb.inverse_transform(predicted)
+
+#prueba
+
+all_labels = getmlb(predicted)
+
+for item, labels in zip(X_test, all_labels):
+    print('{0} => {1}'.format(item, ', '.join(labels)))
+
--- main.py
+++ main.py
@@ -4,12 +4,22 @@ import pickle
 import numpy as np
 from typing import List
 from pydantic import BaseModel
+import algoritms.multiEtiqueta
+
 
 class ItemList(BaseModel):
     data_model: List[float]
 
+class ItemList2(BaseModel):
+    lista:List[str]
+    #data_model2: np.array
+
+#class ItemList2(BaseModel):
+#    data_model: List[np.array]
+
 # Model
 loaded_model = pickle.load(open('./models/Model.pkl', 'rb'))
+loaded_model_ME = pickle.load(open('./models/multietiquetas.pkl', 'rb'))
 
 # inicializar app
 app = FastAPI()
@@ -27,6 +37,16 @@ async def get_item(variables):
 async def predict(data:ItemList):
     result = loaded_model.predict(np.array(data.data_model).reshape(1,4))
     return result[0]
+
+@app.post('/predict/ME/')
+async def predictME(data:ItemList2):
+    result = loaded_model_ME.predict(np.array(data.lista))
+    all_labels = algoritms.multiEtiqueta.mlb.inverse_transform(result)
+    return all_labels
+#prueba
+
+
+    
 
 if __name__ == '__main__':
     uvicorn.run(app, host = '127.0.0.1', port = 8000)
